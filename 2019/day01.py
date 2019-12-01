907cb141dbd3529c7f38459d42154c45b57e85b2
mkurdian <37507230+mkurdian@users.noreply.github.com>
2019-12-01 18:06:16 +0000

    Day 01: Part 02

--- 2019/day01.py
+++ 2019/day01.py
@@ -15,6 +15,23 @@ def sum_func(func, stream):
     return sum(map(func, map(int, stream)))
 
 
+def corrected_fuel_for_mass(mass):
+    """
+    Mass of fuel corrected for fuels own mass.
+    """
+    result = 0
+    fuel = mass
+
+    while fuel >= 0:
+        fuel = fuel_for_mass(fuel)
+        if fuel <= 0:
+            fuel = 0
+            break
+        result += fuel
+
+    return result
+
+
 class TestDay01(unittest.TestCase):
 
     def test_fuel_for_mass(self):
@@ -37,8 +54,20 @@ class TestDay01(unittest.TestCase):
         self.assertEqual(
             sum_func(fuel_for_mass, module_masses), 2 + 2 + 654 + 33583)
 
+    def test_corrected_fuel_for_mass(self):
+        self.assertEqual(corrected_fuel_for_mass(5), 0)
+        self.assertEqual(corrected_fuel_for_mass(21), 5)
+        self.assertEqual(corrected_fuel_for_mass(70), 21 + 5)
+        self.assertEqual(corrected_fuel_for_mass(216), 70 + 21 + 5)
+        self.assertEqual(corrected_fuel_for_mass(1969), 966)
+        self.assertEqual(corrected_fuel_for_mass(100756), 50346)
+
 
 if __name__ == '__main__':
     with open('inputs/input_day01.in') as file:
         part_1_result = sum_func(fuel_for_mass, file)
         print("Part 1: ", part_1_result)
+
+    with open('inputs/input_day01.in') as file:
+        part_2_result = sum_func(corrected_fuel_for_mass, file)
+        print("Part 2: ", part_2_result)
